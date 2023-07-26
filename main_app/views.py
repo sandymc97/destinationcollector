39b59540667dbc9493ef6163a8d8aad97ebe4f3b
Sandy <sandyahmedy7@gmail.com>
2023-07-25 22:05:00 -0500

    html, css, about/destinations routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
+# baby step usually a model is used 
+destinations = [
+  {'location': 'St. Lucia', 'continent': 'North America', 'climate': 'tropical', 'language': 'English'},
+  {'location': 'Costa Rica', 'continent': 'North America', 'climate': 'tropical', 'language': 'Spanish'},
+]
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'about.html')
+
+def destinations_index(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'destinations/index.html', {
+    'destinations': destinations
+  })
